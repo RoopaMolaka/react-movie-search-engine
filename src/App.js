cfd126fc69eea87195933bef91cf3fcaf6553128
MolakaRoopa <roopareddy8978@gmail.com>
2024-02-04 19:33:09 +0530

    movie-search-engine

--- src/App.js
+++ src/App.js
@@ -1,58 +1,86 @@
-import axios from 'axios'
-import React,{useEffect,useState} from 'react'
-import "./App.css"
-import MovieCard from './moviecard'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import MovieCard from "./moviecard";
 
 function App() {
-    const[popularMovies,setPopularMovies]=useState([])
-    const[movieName,setMovieName]=useState("")
-    const[searchedMovies,setSearchedMovies]=useState([])
 
-    useEffect(()=>{
-       axios.get("https://api.themoviedb.org/3/movie/popular?api_key=cfe422613b250f702980a3bbf9e90716")
-       .then((res)=>{
-   setPopularMovies(res.data.results)
-       }).catch((err)=>{
-      console.log(err)
-       })
-    },[])
-    useEffect(()=>{
-        axios.get(`https://api.themoviedb.org/3/search/movie?query=${movieName}&api_key=cfe422613b250f702980a3bbf9e90716`)
-        .then((res)=>{
-            setSearchedMovies(res.data.results)
-        })
+  const [popularMovies, setPopularMovies] = useState([]);
+  const [movieName, setMovieName] = useState("");
+  const [searchedMovies, setSearchedMovies] = useState([]);
 
-    },[movieName])
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.themoviedb.org/3/movie/popular?api_key=cfe422613b250f702980a3bbf9e90716"
+      )
+      .then((res) => {
+        setPopularMovies(res.data.results);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/search/movie?query=${movieName}&api_key=cfe422613b250f702980a3bbf9e90716`
+      )
+      .then((res) => {
+        setSearchedMovies(res.data.results);
+      });
+  }, [movieName]);
 
   return (
     <>
-    <div className='heading'>
-    <h1 >Movie Search Engine</h1>
-   <div id='innerdiv'> <input value={movieName}    placeholder='search movie name'id='input1'
-   onChange={(e)=>{setMovieName(e.target.value)}}/>
-   {/* <button id='btn1-styling'> Go to wishlist</button> */}
-           </div>
-</div>
-<div className='div-wrapper'>
-    { movieName === "" ? popularMovies.map((item,index)=>{
-       return<MovieCard 
-       poster={item.poster_path} 
-       title={item.title} 
-       rating={item.vote_average} 
-       releaseDate={item.release_date}/> 
-
-    }) :searchedMovies.map((item,index)=>{
-      return<MovieCard 
-      poster={item.poster_path} 
-      title={item.title} 
-      rating={item.vote_average} 
-      releaseDate={item.release_date}/>})}
-    
-   </div>
-</>
-    
-  )
+      <div className="heading">
+        <h1 id="mse-heading">Movie Search Engine</h1>
+        <div id="innerdiv">
+          {" "}
+          <input
+            value={movieName}
+            placeholder="search movie name"
+            id="input1"
+            onChange={(e) => {
+              setMovieName(e.target.value);
+            }}
+          />
+          {/* <button
+            id="btn1-styling"
+            onClick={() => {
+              
+            }}
+          >
+            {" "}
+            Go to wishlist{" "}
+          </button> */}
+        </div>
+      </div>
+      <div className="div-wrapper">
+        {movieName === ""
+          ? popularMovies.map((item, index) => {
+              return (
+                <MovieCard key={index}
+                  poster={item.poster_path}
+                  title={item.title}
+                  rating={item.vote_average}
+                  releaseDate={item.release_date}
+                />
+              );
+            })
+          : searchedMovies.map((item, index) => {
+              return (
+                <MovieCard key={index}
+                  poster={item.poster_path}
+                  title={item.title}
+                  rating={item.vote_average}
+                  releaseDate={item.release_date}
+                />
+              );
+            })}
+      </div>
+    </>
+  );
 }
 
-export default App
-
+export default App;
--- src/moviecard.js
+++ src/moviecard.js
@@ -1,16 +1,24 @@
-import React from 'react'
+import React from "react";
 
 function MovieCard(props) {
-    return(<div className='movie-card'>
-    <img className='movie-thumbnail'
-    src={`https:/image.tmdb.org/t/p/w300_and_h450_bestv2${props.poster}`}
-    />
-    <p><b>Title:</b>${props.title}</p>
-    <p><b>Rating:</b>${props.rating}/10</p>
-    <p><b>Release Date:</b>${props.releaseDate}</p>
-    {/* <button id='wishlist-button'>Add to wishlist</button> */}
-     </div> )
-
+  return (
+    <div className="movie-card">
+      <img
+        className="movie-thumbnail"
+        src={`https:/image.tmdb.org/t/p/w300_and_h450_bestv2${props.poster}`}
+      />
+      <p>
+        <b>Title:</b>${props.title}
+      </p>
+      <p>
+        <b>Rating:</b>${props.rating}/10
+      </p>
+      <p>
+        <b>Release Date:</b>${props.releaseDate}
+      </p>
+      <button id="wishlist-button">Add to wishlist</button>
+    </div>
+  );
 }
 
-export default MovieCard
+export default MovieCard;
